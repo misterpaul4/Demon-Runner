3174e72c43c44c62fed6dcb53352094810a58d64
paul <ajuzepaul@gmail.com>
2020-11-23 15:24:45 +0200

    Add Bootscene

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('hello');
+import Phaser from 'phaser';
+import BootScene from './scenes/bootScene';
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 450,
+  scene: [
+    BootScene,
+  ],
+};
+
+const game = new Phaser.Game(config);
---
+++ src/scenes/bootScene.js
@@ -0,0 +1,22 @@
+import Phaser from 'phaser';
+import background from '../assets/background.jpg';
+import startBtn from '../assets/start_btn.png';
+import leaderBoard from '../assets/leaderboard.png';
+
+export default class BootScene extends Phaser.Scene {
+  constructor() {
+    super({
+      key: 'BootScene'
+    });
+  }
+
+  preload() {
+    this.load.image('background', background);
+    this.load.image('start', startBtn);
+    this.load.image('leaderboard', leaderBoard);
+  }
+
+  create() {
+    this.scene.start('PreloaderScene');
+  }
+}
