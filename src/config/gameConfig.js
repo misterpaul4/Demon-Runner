e88bff3de46b41c1de06eeb737a295f6f857e753
paul <ajuzepaul@gmail.com>
2020-11-29 15:14:30 +0200

    Create ground of different sizes

--- src/config/gameConfig.js
+++ src/config/gameConfig.js
@@ -1,12 +1,11 @@
 const gameOptions = {
-  spawnRange: [100, 800],
-  platformSizeRange: [50, 250],
+  groundSpace: 150,
   playerGravity: 900,
   jumpForce: 400,
   jumps: 2,
   gameWidth: 800,
   gameHeight: 450,
-  gameSpeed: 150,
+  gameSpeed: 300,
 };
 
 export default gameOptions;
--- src/scenes/gameScene.js
+++ src/scenes/gameScene.js
@@ -10,17 +10,32 @@ export default class GameScene extends Phaser.Scene {
 
   create() {
     this.add.image(400, 225, 'background').setScrollFactor(0, 1);
-    this.platforms = this.physics.add.staticGroup();
-    // first platform
+    this.platforms = this.physics.add.staticGroup({
+
+      // once a platform is removed, it's added to the pool
+      removeCallback(platform) {
+        platform.scene.platformPool.add(platform);
+      },
+    });
+
+    // pool
+    this.platformPool = this.add.group({
+
+      // once a platform is removed from the pool, it's added to the active platforms group
+      removeCallback(platform) {
+        platform.scene.platforms.add(platform);
+      },
+    });
 
     // const groundY = 420;
     const groundY = 420;
     let groundX = 0;
 
     // add 5 ground
-    for (let i = 0; i < 5; i += 1) {
-      const platform = this.platforms.create(groundX, groundY, 'ground');
-      groundX += (platform.displayWidth + 80);
+    for (let i = 0; i < 25; i += 1) {
+      const platform = this.platforms.create(groundX, groundY, 'ground').setOrigin(0);
+      platform.displayWidth = Phaser.Math.Between(50, 801);
+      groundX += (platform.displayWidth + settings.groundSpace);
 
       const {
         body,
@@ -28,15 +43,15 @@ export default class GameScene extends Phaser.Scene {
       body.updateFromGameObject();
     }
 
-    this.stones = this.physics.add.group({
-      key: 'stone',
-      setXY: {
-        x: 1000,
-        y: 30,
-        stepX: 400,
-      },
-      repeat: 2,
-    });
+    // this.stones = this.physics.add.group({
+    //   key: 'stone',
+    //   setXY: {
+    //     x: 1000,
+    //     y: 30,
+    //     stepX: 400,
+    //   },
+    //   repeat: 2,
+    // });
     this.player = this.physics.add.sprite(50, 350, 'player').setScale(0.1);
 
     this.player.setBounce(0.15);
@@ -45,8 +60,8 @@ export default class GameScene extends Phaser.Scene {
     this.cursors = this.input.keyboard.createCursorKeys();
 
     this.physics.add.collider(this.platforms, this.player);
-    this.physics.add.collider(this.platforms, this.stones);
-    this.physics.add.collider(this.stones, this.player, this.hitStone(), null, this);
+    // this.physics.add.collider(this.platforms, this.stones);
+    // this.physics.add.collider(this.stones, this.player, this.hitStone(), null, this);
 
     // camera.startFollow(gameObject, roundPx, lerpX, lerpY, offsetX, offsetY);
     this.cameras.main.startFollow(this.player, false, 1, 0, -200, 125);
@@ -72,26 +87,49 @@ export default class GameScene extends Phaser.Scene {
     });
 
     this.input.keyboard.on('keydown-SPACE', this.jump, this);
+    this.input.keyboard.on('keydown-UP', this.jump, this);
+
+    this.player.jumps = settings.jumps;
+    // console.log(`first platform = ${this.platforms.getChildren()[1].x}`);
   }
 
   update() {
     this.movement();
   }
 
+  // addPlatform(platformWidth, posX) {
+  //   let platform;
+  //   if (this.platformPool.getLength()) {
+  //     platform = this.platformPool.getFirst();
+  //     platform.x = posX;
+  //     platform.active = true;
+  //     platform.visible = true;
+  //     this.platformPool.remove(platform);
+  //   }
+  //   else {
+  //     platform = this.physics.add.sprite(posX, game.config.height * 0.8, "platform");
+  //     platform.setImmovable(true);
+  //     platform.setVelocityX(gameOptions.platformStartSpeed * -1);
+  //     this.platformGroup.add(platform);
+  //   }
+  //   platform.displayWidth = platformWidth;
+  //   this.nextPlatformDistance = Phaser.Math.Between(gameOptions.spawnRange[0], gameOptions.spawnRange[1]);
+  // }
+
   movement() {
     if (this.player.body.touching.down) {
       this.player.anims.play('run', true);
       this.player.clearTint();
-      settings.jumps = 2;
+      this.player.jumps = settings.jumps;
     }
   }
 
   jump() {
-    if (settings.jumps > 0) {
+    if (this.player.jumps > 0) {
       this.player.setVelocityY(-settings.jumpForce);
       this.player.anims.play('jump', true);
       this.player.setTint(0xff0000);
-      settings.jumps -= 1;
+      this.player.jumps -= 1;
     }
   }
 
